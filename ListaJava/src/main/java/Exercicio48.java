38477cbcb73373f47e3de8a9b01973b5c6bb1347
MatosRicardo <ricardoribeirodematos@gmail.com>
2024-11-27 12:23:11 -0300

    Em andamento

---
+++ ListaJava/src/main/java/Exercicio48.java
@@ -0,0 +1,15 @@
+public class Exercicio48 {
+    public static void main(String[] args) {
+        /*48)Faça um programa que leia um número inteiro e exiba os seus dígitos separadamente usando o loop "while".*/
+        int numero = 12345;
+        int resto;
+
+        System.out.println("Os dígitos do número " + numero + " são:");
+
+        while (numero > 0) {
+            resto = numero % 10;
+            System.out.println(resto);
+            numero = numero / 10;
+        }
+    }
+}
