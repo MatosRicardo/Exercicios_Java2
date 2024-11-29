22ff9594eb53ab628ec445a502df45c74faacb3b
MatosRicardo <ricardoribeirodematos@gmail.com>
2024-11-29 13:07:02 -0300

    Em andamento

---
+++ ListaJava/src/main/java/Exercicio72.java
@@ -0,0 +1,24 @@
+public class Exercicio72 {
+    public static void main(String[] args) {
+        /*72) Faça um programa que leia um número inteiro e exiba se ele é um número primo.*/
+
+        int numero = 29; //Digite o número que voce queira saber se é primo ou não.
+        boolean isPrimo = true;
+
+        if (numero <= 1) {
+            isPrimo = false;
+        } else {
+            for (int i = 2; i < numero; i++) {
+                if (numero % i == 0) {
+                    isPrimo = false;
+                    break;
+                }
+            }
+        }
+        if (isPrimo) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
+}
+}
